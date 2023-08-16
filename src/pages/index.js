4faec5b5449c48d11d6cfe096d3634f948196ccc
British Berean <britishberean@protonmail.com>
2023-08-16 23:13:46 +0100

    Enable clients section again with error boundary

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import { Grid, Box } from '@mui/material';
+import { ErrorBoundary } from 'react-error-boundary';
 import Layout from '../components/Layout';
 import Intro from '../components/index/Intro';
 import Services from '../components/index/Services';
 import CaseStudies from '../components/index/CaseStudies';
-// import Clients from '../components/index/Clients';
+import Clients from '../components/index/Clients';
 import Contact from '../components/index/Contact';
 
 const IndexPage = ({ location }) => {
-  const sections = [Intro, Services, CaseStudies, /* Clients,*/ Contact];
+  const sections = [Intro, Services, CaseStudies, Clients, Contact];
   return (
     <Layout location={location}>
       <Grid container>
         {sections.map((Section, index) => (
           <Grid item xs={12} key={index}>
             <Box sx={{ backgroundColor: index % 2 ? 'background.default' : 'background.dark'}}>
-              <Section key={index} />
+              <ErrorBoundary fallback={({ error }) => {
+                console.log(error);
+                return (
+                  <Box></Box>
+                );
+              }}>
+                <Section key={index} />
+              </ErrorBoundary>
             </Box>
           </Grid>
         ))}
