38594c18c1085e0b4c7972202b2313ef4abfb101
British Berean <britishberean@protonmail.com>
2023-08-16 15:53:55 +0100

    Update Case Studies

--- src/components/index/CaseStudies.jsx
+++ src/components/index/CaseStudies.jsx
@@ -1,29 +1,40 @@
 import React from 'react';
-import { Grid, Typography, Button, Box } from '@mui/material';
+import { Grid, Typography, Button } from '@mui/material';
 import caseOneImage from '../../images/case-studies/office-concept-illustration.jpg';
 import caseTwoImage from '../../images/case-studies/flat-people-business-training-illustrated.jpg';
+import caseThreeImage from '../../images/case-studies/office-background-video-conferencing.jpg';
 
 const CaseStudiesSection = () => (
   <Grid item id="case-studies">
     <Grid container p={2}>
       <Grid item container xs={12}>
         <Grid item xs={12} sm={6}>
-          <Typography variant="h4" pl={4} pr={4} pt={2}>Case study #1</Typography>
-          <Typography p={4}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Typography>
+          <Typography variant="h5" pl={4} pr={4} pt={2}>Case Study #1: Unlocking Paid Social with Virgin Experience Holidays</Typography>
+          <Typography p={4}>We took our unique lateral audience strategy to task for Virgin, looking at users overlapping interests as well as direct advertising. Over the first two months, we achieved a 145% increase in site visitors from social media sources, and a 13% increase in transaction volume.</Typography>
           <Button sx={{ ml: 4 }} variant="contained" color="secondary">Read More</Button>
         </Grid>
         <Grid item xs={12} sm={6} sx={{ textAlign: 'center' }}>
-          <img src={caseOneImage} height={300} style={{ padding: '32px', borderRadius: '64px' }} />
+          <img alt="Case Study 1" src={caseOneImage} height={300} style={{ padding: '32px', borderRadius: '64px' }} />
         </Grid>
       </Grid>
       <Grid item container xs={12}>
         <Grid item xs={12} sm={6} sx={{ textAlign: 'center' }}>
-            <img src={caseTwoImage} height={300} style={{ padding: '32px', borderRadius: '64px' }} />
+          <img alt="Case Study 2" src={caseTwoImage} height={300} style={{ padding: '32px', borderRadius: '64px' }} />
         </Grid>
         <Grid item xs={12} sm={6}>
-          <Typography variant="h4" pl={4} pr={4} pt={2}>Case study #2</Typography>
-          <Typography p={4}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Typography>
+          <Typography variant="h5" pl={4} pr={4} pt={2}>Case Study 2: SEO for FirstVet</Typography>
+          <Typography p={4}>FirstVet approached us looking to improve their organic search coverage. By deeply analysing niche keywords for services that competitors were ignoring, We ran a campaign that increased organic click volume 14,000% over 6 months and improved overall page rankings by 179%.</Typography>
           <Button sx={{ ml: 4 }} variant="contained" color="secondary">Read More</Button>
+        </Grid>
+      </Grid>
+      <Grid item container xs={12}>
+        <Grid item xs={12} sm={6}>
+          <Typography variant="h5" pl={4} pr={4} pt={2}>Case Study 3: Conversion Rate Optimisation for Wax London</Typography>
+          <Typography p={4}>Wax London were one of our first clients, bringing quality men’s fashion to an international audience. Our competitive strategy on paid social channels and creative feedback process improved site revenues year on year by 234% for paid channels, and 284% across their entire site.</Typography>
+          <Button sx={{ ml: 4 }} variant="contained" color="secondary">Read More</Button>
+        </Grid>
+        <Grid item xs={12} sm={6} sx={{ textAlign: 'center' }}>
+          <img alt="Case Study 3" src={caseThreeImage} height={300} style={{ padding: '32px', borderRadius: '64px' }} />
         </Grid>
       </Grid>
     </Grid>
