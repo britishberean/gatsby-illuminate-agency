8a5c16fbf50b2ed9ea6153b52dff14ec7fb9198a
British Berean <britishberean@protonmail.com>
2023-08-16 16:02:28 +0100

    Update Services

--- src/components/index/Services.jsx
+++ src/components/index/Services.jsx
@@ -15,25 +15,19 @@ const ServicesSection = () => (
       <Grid item xs={12} sm={6} lg={4}>
         <Paper sx={{ m: 4 }}>
           <Typography variant="h6" p={1}>Boost Your Online Visibility with SEO</Typography>
-          <Typography p={1}>Boost your website's visibility and drive more organic traffic with our SEO services - get ready to dominate search engine rankings!</Typography>
+          <Typography p={1}>Increase your website's search visibility and drive more organic traffic with our full range of SEO services. From domain authority boosting and website technical consultancy, to content relevancy analysis - we have the expertise to help you dominate search engine rankings!</Typography>
         </Paper>
       </Grid>
       <Grid item xs={12} sm={6} lg={4}>
         <Paper sx={{ m: 4 }}>
-          <Typography variant="h6" p={1}>Social Media Magic - Boost Your Brand!</Typography>
-          <Typography p={1}>Our SMM service helps businesses boost their online presence and engage with their audience through strategic social media management.</Typography>
+          <Typography variant="h6" p={1}>Paid Social Media Campaigns - Grow Your Brand!</Typography>
+          <Typography p={1}>We have years of experience helping a variety of brands create social media strategies to grow their online presence and achieve results. Our social media management will allow you to engage your audience through creative and eye-catching campaigns and maximise performance through retargeting strategies.</Typography>
         </Paper>
       </Grid>
       <Grid item xs={12} sm={6} lg={4}>
         <Paper sx={{ m: 4 }}>
-          <Typography variant="h6" p={1}>Boost Your Brand with Engaging Content</Typography>
-          <Typography p={1}>Our content marketing service helps you create engaging and shareable content that connects with your target audience in a relaxed and friendly way.</Typography>
-        </Paper>
-      </Grid>
-      <Grid item xs={12} sm={6} lg={4}>
-        <Paper sx={{ m: 4 }}>
-          <Typography variant="h6" p={1}>Boost Your Leads with Expertise</Typography>
-          <Typography p={1}>Need more leads for your business? We've got you covered! Out lead generation service helps you find porential customers and grow your sales.</Typography>
+          <Typography variant="h6" p={1}>PPC Account Management - Take Advantage of Search Opportunities</Typography>
+          <Typography p={1}>Capture audience interest and accelerate business growth with our PPC account management. We'll help you to identify relevant search terms and optimise ad campaigns to engage users and direct traffic to your website. We also have extensive experience using AI-powered solutions to find new customers and unlock additional performance.</Typography>
         </Paper>
       </Grid>
     </Grid>
