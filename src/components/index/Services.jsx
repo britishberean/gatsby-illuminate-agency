3fc9cff6c7418558fd13995eaf7797076b206fe7
British Berean <britishberean@protonmail.com>
2023-09-12 10:18:42 +0100

    Alter order of services boxes

--- src/components/index/Services.jsx
+++ src/components/index/Services.jsx
@@ -20,14 +20,14 @@ const ServicesSection = () => (
       </Grid>
       <Grid item xs={12} sm={6} lg={4}>
         <Paper sx={{ m: 4 }}>
-          <Typography variant="h6" p={1}>Paid Social Media Campaigns - Grow Your Brand!</Typography>
-          <Typography p={1}>We have years of experience helping a variety of brands create social media strategies to grow their online presence and achieve results. Our social media management will allow you to engage your audience through creative and eye-catching campaigns and maximise performance through retargeting strategies.</Typography>
+          <Typography variant="h6" p={1}>PPC Account Management - Take Advantage of Search Opportunities</Typography>
+          <Typography p={1}>Capture audience interest and accelerate business growth with our PPC account management. We'll help you to identify relevant search terms and optimise ad campaigns to engage users and direct traffic to your website. We also have extensive experience using AI-powered solutions to find new customers and unlock additional performance.</Typography>
         </Paper>
       </Grid>
       <Grid item xs={12} sm={6} lg={4}>
         <Paper sx={{ m: 4 }}>
-          <Typography variant="h6" p={1}>PPC Account Management - Take Advantage of Search Opportunities</Typography>
-          <Typography p={1}>Capture audience interest and accelerate business growth with our PPC account management. We'll help you to identify relevant search terms and optimise ad campaigns to engage users and direct traffic to your website. We also have extensive experience using AI-powered solutions to find new customers and unlock additional performance.</Typography>
+          <Typography variant="h6" p={1}>Paid Social Media Campaigns - Grow Your Brand!</Typography>
+          <Typography p={1}>We have years of experience helping a variety of brands create social media strategies to grow their online presence and achieve results. Our social media management will allow you to engage your audience through creative and eye-catching campaigns and maximise performance through retargeting strategies.</Typography>
         </Paper>
       </Grid>
     </Grid>
